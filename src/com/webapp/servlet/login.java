5be6205fc51c9dbcecf137386043490f2313226e
Bien Christian B. Custodio <bbcustodio@student.apc.edu.ph>
2017-10-23 13:55:30 +0800

    MySQL Initial Connection

---
+++ src/com/webapp/servlet/login.java
@@ -0,0 +1,42 @@
+package com.webapp.servlet;
+
+import com.webapp.model.User;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.awt.*;
+import java.io.PrintWriter;
+/**
+ * Created by Ian on 20/10/2017.
+ */
+@WebServlet(name = "login")
+public class login extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        User user = new User();
+        if (user.login(username, password)) {
+            HttpSession session = request.getSession();
+            session.setAttribute("isLogged", true);
+            request.getRequestDispatcher("index.jsp").forward(request, response);
+        } else {
+            request.getRequestDispatcher("login.jsp").forward(request, response);
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        try {
+            String driverName = "com.mysql.jdbc.driver";
+            Class.forName(driverName);
+            out.println("Driver Loaded...");
+        } catch (Exception e) {
+            out.println("Driver cannot be found...");
+        }
+    }
+}
